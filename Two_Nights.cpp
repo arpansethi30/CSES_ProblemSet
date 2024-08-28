597da962235169dc7e27c820419bfc50d710cba1
Arpan Sethi <arpansethi30@gmail.com>
2024-08-29 01:38:53 +0530

    Two_Nights_Correct

--- Two_Nights.cpp
+++ Two_Nights.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
-
 using namespace std;
 
-long CalculateWays(int k)
+// Function to calculate and print the number of ways two
+// knights can be placed on a K X K chessboard such that
+// they do not attack each other
+long calculateWays(int K)
 {
-    long total_ways = ((long)k * k * k * (k - 1)) / 2;
-    long way_attack = 4 * (k - 1) * (k - 2);
-    long ans = total_ways - way_attack;
+    // Total number of ways two knights can be placed on
+    // the chessboard
+    long totalWays = ((long)K * K * (K * K - 1)) / 2;
+
+    // Number of ways two knights can attack each other
+    long attackingWays = 4 * (K - 1) * (K - 2);
+
+    // Number of ways two knights can be placed without
+    // attacking each other
+    long ans = totalWays - attackingWays;
+
+    // Return the result for the current chessboard size K
     return ans;
 }
 
+// Driver Code
 int main()
 {
-    int k;
-    cin >> k;
-    for (int i = 1; i <= k; i++)
+    // Input the value of N (size of the chessboard)
+    int N;
+    cin >> N;
+
+    // Iterate for all the K sized chessboard
+    for (int K = 1; K <= N; K++)
     {
-        cout << CalculateWays(i) << endl;
+        cout << calculateWays(K) << " ";
     }
+
     return 0;
 }
 
